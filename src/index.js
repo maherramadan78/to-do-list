27a67ed6c85f608e18383cc5a61ed5d21216b689
maher <maherramadan2012@hotmail.com>
2022-12-24 14:54:53 -0600

    Implement add and remove functionality

--- src/index.js
+++ src/index.js
@@ -1,31 +1,44 @@
 import './style.css';
+import {
+  addItem,
+  removeItem,
+  displayToDos,
+  getDescriptionInput,
+} from './CRUD.js';
 
 const todoContainer = document.querySelector('.todos-container');
+const addBtn = document.querySelector('.fa-plus');
+const input = document.querySelector('.toDoName');
+const locStorage = JSON.parse(localStorage.getItem('todos'));
+let arr;
+if (locStorage == null) {
+  arr = [];
+} else {
+  arr = locStorage;
+}
+window.addEventListener('load', () => {
+  displayToDos(arr, todoContainer);
+});
+addBtn.addEventListener('click', () => {
+  const todoContainer = document.querySelector('.todos-container');
+  const inputVal = input.value;
+  addItem(arr, inputVal);
+  const locStorage = JSON.parse(localStorage.getItem('todos'));
+  input.value = '';
+  displayToDos(locStorage, todoContainer);
+});
 
-const arr = [{
-  description: 'wash the dishes',
-  completed: false,
-  index: 0,
-}, {
-  description: 'complete your assignment',
-  completed: true,
-  index: 1,
-}, {
-  description: 'complete the todo list',
-  completed: false,
-  index: 2,
-}];
+todoContainer.addEventListener('click', (e) => {
+  if (e.target.tagName === 'I' && e.target.classList.value === 'fas fa-trash-alt') {
+    const { id } = e.target;
+    removeItem(id);
+    const locStorage = JSON.parse(localStorage.getItem('todos'));
+    displayToDos(locStorage, todoContainer);
+  } else if (e.target.tagName === 'INPUT' && e.target.type !== 'checkbox') {
+    e.target.readOnly = false;
 
-const displayToDos = () => {
-  arr.forEach((item) => {
-    todoContainer.innerHTML += `<li class="todos widthHeight">
-        <ul class="todos-01">
-        <li><input type="checkbox" class=" toDoItems"></li>
-        <li><p class="toDoItems">${item.description}</p></li>
-        </ul>
-        <i class="fas fa-ellipsis-v"></i>
-    </li>`;
-  });
-};
-
-window.onresize.apply = displayToDos();
+    const { id } = e.target;
+    const arr = JSON.parse(localStorage.getItem('todos'));
+    getDescriptionInput(e.target, arr, id);
+  }
+});
