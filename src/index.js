80fb59412a80669883440dda2d8ad37e2f3d43fa
maher <maherramadan2012@hotmail.com>
2023-03-01 11:43:52 -0600

    Create unit test.jest

--- src/index.js
+++ src/index.js
@@ -1,44 +1,38 @@
 import './style.css';
+import { addNewTask } from './modules/addItem.js';
+import modifyTask from './modules/editTask.js';
+import { removeTask } from './modules/deleteTask.js';
+import removeCompleted from './modules/clearCompleted.js';
+import completeTask from './modules/markTask.js';
+import { displayListTasks } from './modules/listCard.js';
+
 import {
-  addItem,
-  removeItem,
-  displayToDos,
-  getDescriptionInput,
-} from './CRUD.js';
+  updateLocalStorage,
+  getLocalStorage as tasksList,
+} from './modules/data.js';
 
-const todoContainer = document.querySelector('.todos-container');
-const addBtn = document.querySelector('.fa-plus');
-const input = document.querySelector('.toDoName');
-const locStorage = JSON.parse(localStorage.getItem('todos'));
-let arr;
-if (locStorage == null) {
-  arr = [];
-} else {
-  arr = locStorage;
-}
-window.addEventListener('load', () => {
-  displayToDos(arr, todoContainer);
-});
-addBtn.addEventListener('click', () => {
-  const todoContainer = document.querySelector('.todos-container');
-  const inputVal = input.value;
-  addItem(arr, inputVal);
-  const locStorage = JSON.parse(localStorage.getItem('todos'));
-  input.value = '';
-  displayToDos(locStorage, todoContainer);
+// Declare variables
+const input = document.querySelector('.inputField');
+const todoList = document.querySelector('.toDoContainer');
+const addTaskBtn = document.querySelector('.insert-btn');
+
+const clearCompletedBtn = document.querySelector('.clearbtn');
+
+// ADD TASK EVENT LISTENER FOR CLICK EVENT
+
+addTaskBtn.addEventListener('click', () => {
+  if (input.value !== '') {
+    addNewTask(tasksList(), input.value);
+    input.value = '';
+    updateLocalStorage(tasksList());
+    displayListTasks(tasksList());
+  }
 });
 
-todoContainer.addEventListener('click', (e) => {
-  if (e.target.tagName === 'I' && e.target.classList.value === 'fas fa-trash-alt') {
-    const { id } = e.target;
-    removeItem(id);
-    const locStorage = JSON.parse(localStorage.getItem('todos'));
-    displayToDos(locStorage, todoContainer);
-  } else if (e.target.tagName === 'INPUT' && e.target.type !== 'checkbox') {
-    e.target.readOnly = false;
+// DELETE TASK
 
-    const { id } = e.target;
-    const arr = JSON.parse(localStorage.getItem('todos'));
-    getDescriptionInput(e.target, arr, id);
+todoList.addEventListener('click', (event) => {
+  if (event.target.closest('.trash-can')) {
+    removeTask(event, tasksList());
   }
 });
